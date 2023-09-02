bb02b0dcfdc776997fb234b2c8dc4ffc0adfe5ea
Adil Shamji <adil02.shamji@gmail.com>
2023-09-03 01:02:57 +0200

    ny

--- analys.py
+++ analys.py
@@ -1,25 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
-import io
 import json
-import codecs
 import ast
-import time
+
 url_aftonbladet  ="https://rss.aftonbladet.se/rss2/small/pages/sections/senastenytt/"
 url_svt = "https://www.svt.se/nyheter/rss.xml"
 url_svd = "https://www.svd.se/?service=rss"
-
-
 
 def get_data(url):
     xml = requests.get(url)
     soup = BeautifulSoup(xml.content, features="xml")
     items = soup.find_all("item")
-    list_news = {}
+    list_news = []
     counter = 0
     for item in items:
+        temp_dict = {}
         counter += 1
-        if counter == 2:
+        if counter == 1:
             break
         try:
             description = item.find("description").text
@@ -40,7 +37,6 @@ def get_data(url):
         if url == url_aftonbladet:
             #Ta bort den där symbolen från aftonbladet
             description = description[2:]
-        
             #slippa råka klicka på länk.
         link = item.find("link").text
         link = "https...."
@@ -50,7 +46,9 @@ def get_data(url):
             news_paper = "SVD"
         elif url == url_svt:
             news_paper = "SVT"
-        list_news[item.find("title").text]=([description,link,news_paper])
+        
+        temp_dict[item.find("title").text]=[description,link,news_paper]
+        list_news.append(temp_dict)
     return list_news
 
 def write_file(data):
@@ -66,57 +64,41 @@ def read_file():
         file_contents_list =[]
         for line in file:
             line = line.strip()
-            if not line:  # line is blank
+            if not line or line.startswith("#"): 
+                print("FACJJJ")
                 continue
-            if line.startswith("#"):  # comment line
-                continue
-            data = line.split(',')
             file_contents  = line
             file_contents = ast.literal_eval(file_contents)
-            file_contents_list.append(file_contents)
+            file_contents_list.append([file_contents])
 
     # Clear the file by opening it in write mode and truncating its content
-    # with open("data.txt", "w", encoding="utf-8") as file:
-    #     file.write("")
+    with open("data.txt", "w", encoding="utf-8") as file:
+        file.write("")
     return file_contents_list
 
-def compare_data(url_list):
+def compare_data(url_list, list_data_file):
     #Läs in från filen först, list_rss ska jämföras med list_data_file
-    list_data_file = read_file()
-
-    list_rssdata=[get_data(url) for url in url_list]
+    
+    list_rssdata = [get_data(url) for url in url_list]
     list_merged = list_rssdata + list_data_file
-    # print(list_merged)
-
-
     list_merged = [i for n, i in enumerate(list_merged)
                if i not in list_merged[n + 1:]]
-    
-    
-    #print(list_merged)
-    print("MERGED EFTER")
-    print("")
-    print(list_merged)
+
+    for row in list_merged:                                 #Detta borde egentligen vara i write_file. Låt funktionen göra en sak
+        if (type(row) == dict):
+            write_file(row)
+        for data in row:
+            if (type(data) == dict):
+                write_file(data)    
+    return list_merged           
+
+def main1(url):
+    list_data = read_file()
+    list_merged = compare_data(url, list_data)
+    return list_merged
 
 
-    for data in list_merged:
-        pass
-        #print(data)
-        # print(type(data))
-        # if isinstance(data, list):
-        #     print(data)      
-    #     for dict in data.items():
-    #         write_file(dict)
-        # time.sleep(3)
-    
-
-def main(url):
-    compare_data(url)
-
-
-
-#list_url = [url_aftonbladet,url_svt, url_svd]
-list_url = [url_aftonbladet]
-
-main(list_url)
+list_url = [url_aftonbladet,url_svt, url_svd]
+#list_url = [url_aftonbladet]
+main1(list_url)
 
--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 from sentence_transformers import SentenceTransformer, util
+from analys import main1, list_url
 model = SentenceTransformer("KBLab/sentence-bert-swedish-cased")
 
 
+def main():
+    list_merged = main1(list_url)
+    print(list_merged)
+    sentences2 = ""
+    for i,lists in enumerate(list_merged):
+        for dict in lists:
+            for value in dict.values():
+                sentences1 = value[0]
+                for iter in range(len(value)):
+                sentences2 = 
+            
 
-sentences1 = ["plugga"]
-sentences2 = ["dugga"]
+    sentences1 = ["plugga"]
+    sentences2 = ["dugga"]
 
 
-#Compute embedding for both lists
-embeddings1 = model.encode(sentences1, convert_to_tensor=True)
-embeddings2 = model.encode(sentences2, convert_to_tensor=True)
+    #Compute embedding for both lists
+    embeddings1 = model.encode(sentences1, convert_to_tensor=True)
+    embeddings2 = model.encode(sentences2, convert_to_tensor=True)
 
-#Compute cosine-similarities
-cosine_scores = util.cos_sim(embeddings1, embeddings2)
+    #Compute cosine-similarities
+    cosine_scores = util.cos_sim(embeddings1, embeddings2)
 
-#Output the pairs with their score
-for i in range(len(sentences1)):
-    print("{} \t\t {} \t\t Score: {:.2f}".format(sentences1[i], sentences2[i], cosine_scores[i][i]))
+    #Output the pairs with their score
+    for i in range(len(sentences1)):
+        print("{} \t\t {} \t\t Score: {:.2f}".format(sentences1[i], sentences2[i], cosine_scores[i][i]))
 
+
+main()
